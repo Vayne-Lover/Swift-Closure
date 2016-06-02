ff3ec50d20a023608cf626bc8d07932276e2ef2f
Vayne-Lover <406378362@qq.com>
2016-06-02 09:44:22 +0800

    Show examples

--- Closure Example.playground/Contents.swift
+++ Closure Example.playground/Contents.swift
@@ -1,5 +1,35 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-var str = "Hello, playground"
+func ShowClosure0() -> (Int,Int)->Bool {//Use function
+    let result:(Int,Int)->Bool
+    result={(a:Int,b:Int)->Bool in
+        return a<b
+    }
+    return result
+}
+func ShowClosure1() -> (Int,Int)->Bool  {//Decrease type of parameters
+    let result:(Int,Int)->Bool
+    result={(a,b) in a<b}
+    return result
+}
+func ShowClosure2() -> (Int,Int)->Bool  {//Change to $0,$1
+    let result:(Int,Int)->Bool
+    result={$0<$1}
+    return result
+}
+let example:[Int] = [1,5,6,3,8]
+print(example.sort(ShowClosure0()))
+print(example.sort(ShowClosure1()))
+print(example.sort(ShowClosure2()))
+print(example.sort(
+    {
+        (a:Int,b:Int)->Bool in
+        return a<b
+    }
+    )
+)
+print(example.sort(
+    {(a,b) in a<b}//Delete return
+    )
+)
+print(example.sort({$0<$1}))
+print(example.sort(){$0<$1})
+print(example.sort(<))
